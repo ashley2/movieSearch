f69892e556b8de47d9c5aa51a5d699ce409e7df6
Ashley Smith <ashley22@vt.edu>
2016-03-01 09:24:33 -0800

    updated style

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ $(init);
 
 function init() {
 
+  var popCorn = $('<div>').addClass('pop animated zoomIn')
+  $('#popcorn').append(popCorn);
 
 
   $('#getMovie').click(getMovie);
@@ -15,39 +17,49 @@ function init() {
 
 
 function  getMovie(){
-   console.log('click')
-  var $movieTitle = $('#titleInput').val();
-  var $movieType = $('#typeOf').val();
-  console.log($movieType)
-  console.log($movieYear)
-  var $movieYear = $('#yearOf').val();
-  var url = `http://www.omdbapi.com/?t=${$movieTitle.split(" ").join("+")}&type=${$movieType}&y=${$movieYear}&r=json`;
 
-  $.get(url)
-  .success(function(data){
-    console.log(data)
-    var title = data.Title;
-    var type = data.Type;
-    var year = data.Year;
-    var siteID = data.imdbID
-    var posterURL = data.Poster;
-    var posterImage = $('<img>').attr('src', posterURL);
+ var $movieTitle = $('#titleInput').val();
+ var $movieType = $('#typeOf').val();
+ var $movieYear = $('#yearOf').val();
+ var url = `http://www.omdbapi.com/?t=${$movieTitle.split(" ").join("+")}&type=${$movieType}&y=${$movieYear}&r=json`;
 
-console.log(siteID);
+ $.get(url)
+ .success(function(data){
+
+ 
+  $('#popcorn').remove();
 
 
-    $('#title').text(title);
-    $('#year').text(year);
-    $('#type').text(type);
-    $('#poster').append(posterImage);
-    $('siteID').text(siteID);
 
-    if (data.Response==="False"){
-      alert('movie not found');
-    }
+  var title = data.Title;
+  var type = data.Type;
+  var year = data.Year;
+  var siteID =  `<a href=http://www.imdb.com/title/${data.imdbID} >IMDb</a>`;
+  var posterURL = data.Poster;
+  var posterImage = $('<img>').attr('src', posterURL);
 
-  })
-  .error(function(err){
-    console.log(err);
-  });
+
+
+
+  $('#title').text(title);
+  $('#year').text(year);
+  $('#type').text(type);
+  $('#poster').attr('src', posterURL);
+  $('#poster').addClass('shadow');
+  $('#siteID').html(siteID);
+
+
+
+  if (data.Response==="False"){
+    swal("Oops! Looks like we can't find this movie! Try another!");
+  }
+
+  $('#titleInput').val(' ');
+  $('#yearOf').val(' ');
+
+
+})
+ .error(function(err){
+  console.log(err);
+});
 }
